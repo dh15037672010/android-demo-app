bf1ac418d5e4834d2791adf2ae0d908cc78d2614
热心网友丁同学 <2280131807@qq.com>
2021-01-09 19:07:19 +0800

    Update WelcomeActivity.java

--- PyTorchDemoApp/app/src/main/java/org/pytorch/demo/WelcomeActivity.java
+++ PyTorchDemoApp/app/src/main/java/org/pytorch/demo/WelcomeActivity.java
@@ -2,7 +2,7 @@ package org.pytorch.demo;
 
 import android.Manifest;
 import android.content.Intent;
-import android.content.pm.PackageManager;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
